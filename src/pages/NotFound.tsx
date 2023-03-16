1eb7ead79ad7324b207d30edd69d94d1cb4d7fc6
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2023-03-16 10:16:50 +0300

    feat: styles for 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 
 export default class NotFound extends React.Component {
   render() {
+    const style = {
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: 'var(--fs-xl)',
+      fontWeight: 'var(--fw-bold)',
+    };
     return (
       <Wrapper>
-        <div>NotFound</div>
+        <div style={style}>Not Found</div>
       </Wrapper>
     );
   }
