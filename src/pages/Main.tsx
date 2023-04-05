06625b635660484e47d1b821cb1f34752ee34a83
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2023-04-05 11:48:03 +0300

    refactor: apply linter

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Wrapper, Search, CardsList, Loader, Modal } from '../components';
+import { Wrapper, Search, CardsList, Loader } from '../components';
 import { ImageItem, SearchResponse } from '../types';
 
 const API_URL = 'https://api.unsplash.com/search/photos?per_page=12&query=';
