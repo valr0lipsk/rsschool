cc0e423a4cf834b388a51df08205f742d6f4e1e0
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2023-04-12 12:13:51 +0300

    refactor: refactor store for tests

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
 import cardsSlice from './features/cards/cardsSlice';
 import usersSlice from './features/users/usersSlice';
 
-export const store = configureStore({
-  reducer: {
-    cards: cardsSlice,
-    users: usersSlice,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+export const rootReducer = combineReducers({
+  cards: cardsSlice,
+  users: usersSlice,
 });
 
-export type AppState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export function setupStore(preloadedState?: PreloadedState<AppState>) {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+    preloadedState,
+  });
+}
+
+export type AppState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
